5203df0f76f50eff76ab9798fd374c1ab78f4e90
Fawaz Ahmed <fawazahmed0@hotmail.com>
2019-11-14 12:37:50 +0530

    Change admin previleges code

--- Latest-ADB-Installer.bat
+++ Latest-ADB-Installer.bat
@@ -2,34 +2,7 @@
 
 :: Source: https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
 :: batch code to request admin previleges
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 :: Source: https://stackoverflow.com/questions/4619088/windows-batch-file-file-download-from-a-url
 :: Downloading the latest platform tools from google
