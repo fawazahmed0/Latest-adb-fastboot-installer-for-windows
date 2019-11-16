f1723a4bc5ca1641f406a465ad74a03161b91d28
Fawaz Ahmed <fawazahmed0@hotmail.com>
2019-11-16 21:51:23 +0530

    Add usb driver installation

--- Latest-ADB-Installer.bat
+++ Latest-ADB-Installer.bat
@@ -9,8 +9,17 @@ echo ====================================================
 echo.
 
 :: Source: https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
-:: batch code to request admin previleges
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+:: Source: https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
+:: batch code to request admin previleges, if no admin previleges
+net session >nul 2>&1
+if NOT %errorLevel% == 0 (
+powershell start -verb runas '%0' am_admin & exit /b
+)
+
+
+:: Source: https://stackoverflow.com/questions/672693/windows-batch-file-starting-directory-when-run-as-admin
+:: Going back to script directory
+cd %~dp0
 
 :: Source: https://serverfault.com/questions/132963/windows-redirect-stdout-and-stderror-to-nothing
 :: Null stdout redirection
@@ -30,7 +39,9 @@ PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://dl.google.
 
 echo Downloading latest usb drivers
 PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://dl.google.com/android/repository/latest_usb_driver_windows.zip', 'google_usb_driver.zip')"
-PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://online.mediatek.com/Public Documents/MTK_Android_USB_Driver.zip', 'mtk_usb_driver.zip')"
+PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/fawazahmed0/Latest-adb-fastboot-installer-for-windows/master/files/google64inf', 'google64inf')"
+PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/fawazahmed0/Latest-adb-fastboot-installer-for-windows/master/files/google86inf', 'google86inf')"
+
 
 :: Uninstalling/removing the platform tools older version, if they exists and  killing instances of adb if they are running
 echo Uninstalling older version
@@ -43,20 +54,39 @@ echo Installing the files
 PowerShell -Command "& {$shell_app=new-object -com shell.application; $filename = \"adbinstallerpackage.zip\"; $zip_file = $shell_app.namespace((Get-Location).Path + \"\$filename\"); $destination = $shell_app.namespace(\"C:\Program Files\"); $destination.Copyhere($zip_file.items());}"
 echo Installing USB drivers
 PowerShell -Command "& {$shell_app=new-object -com shell.application; $filename = \"google_usb_driver.zip\"; $zip_file = $shell_app.namespace((Get-Location).Path + \"\$filename\"); $destination = $shell_app.namespace((Get-Location).Path); $destination.Copyhere($zip_file.items());}"
-PowerShell -Command "& {$shell_app=new-object -com shell.application; $filename = \"mtk_usb_driver.zip\"; $zip_file = $shell_app.namespace((Get-Location).Path + \"\$filename\"); $destination = $shell_app.namespace((Get-Location).Path); $destination.Copyhere($zip_file.items());}"
 
-::Using Mediatek .inf file
-copy MTK_Android_USB_Driver\*.inf usb_driver\*.inf /Y > nul 2>&1
 
-:: Source: https://stackoverflow.com/questions/22496847/installing-a-driver-inf-file-from-command-line
-:: Installing USB Drivers
-cd usb_driver
-pnputil -i -a *.inf > nul 2>&1
+
+:: Source: https://github.com/koush/UniversalAdbDriver
+:: Source: https://forum.xda-developers.com/google-nexus-5/development/adb-fb-apx-driver-universal-naked-t2513339
+:: Source: https://stackoverflow.com/questions/60034/how-can-you-find-and-replace-text-in-a-file-using-the-windows-command-line-envir
+:: Combining multiple inf Files to support all the devices
+powershell -Command "(gc usb_driver\android_winusb.inf) -replace '\[Google.NTamd64\]', (gc google64inf) | Out-File usb_driver\android_winusb.inf"
+powershell -Command "(gc usb_driver\android_winusb.inf) -replace '\[Google.NTx86\]', (gc google86inf) | Out-File usb_driver\android_winusb.inf"
+
+:: Fetching unsigned driver installer tool
+echo Downloading unsigned driver installer tool
+PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/fawazahmed0/windows-unsigned-driver-installer/master/unsigned_driver_installer.bat', 'usb_driver\unsigned_driver_installer.bat')"
+
+:: Source: https://stackoverflow.com/questions/1103994/how-to-run-multiple-bat-files-within-a-bat-file
+:: https://stackoverflow.com/questions/3583565/how-to-skip-pause-in-batch-file
+:: Running unsigned_driver_installer tool
+pushd usb_driver
+echo | call unsigned_driver_installer.bat
+popd
 
 :: Source: https://stackoverflow.com/questions/51636175/using-batch-file-to-add-to-path-environment-variable-windows-10
+:: Source: https://stackoverflow.com/questions/141344/how-to-check-if-directory-exists-in-path/8046515
+:: Source: https://stackoverflow.com/questions/9546324/adding-directory-to-path-environment-variable-in-windows
 :: Setting the path Environment Variable
 echo Setting the Environment
-setx path "C:\Program Files\platform-tools;%path%;" > nul 2>&1
+SET Key="HKCU\Environment"
+FOR /F "usebackq tokens=2*" %%A IN (`REG QUERY %Key% /v PATH`) DO Set CurrPath=%%B
+echo ;%CurrPath%; | find /C /I ";C:\Program Files\platform-tools;" > temp.txt
+set /p VV=<temp.txt
+if "%VV%" EQU "0" (
+SETX PATH "C:\Program Files\platform-tools;%CurrPath%" > nul 2>&1
+)
 
 :: Deleting the temporary directory
 echo Deleting the temporary folder
