e974576d67a216e940487f79183b7c98265ea08a
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-08-14 17:19:21 +0530

    Change timings in launcher

--- files/Latest ADB Fastboot Launcher.bat
+++ files/Latest ADB Fastboot Launcher.bat
@@ -2,17 +2,18 @@
 mkdir "%HOMEPATH%\Latest ADB Fastboot Tool"  > nul 2>&1
 
 cd "%HOMEPATH%\Latest ADB Fastboot Tool"  > nul 2>&1
-
 echo.
+echo Please do consider to donate and share this tool
+echo Donate Link: fawazahmed0.github.io/donate.html
+PowerShell -Command "Start-Sleep -s 1" > nul 2>&1
+echo.
+
 echo Paste your files(twrp,etc) in Latest ADB Fastboot Tool Folder
 echo and enter ADB and Fastboot commands in here
+PowerShell -Command "Start-Sleep -s 3" > nul 2>&1
 echo.
 
-PowerShell -Command "Start-Sleep -s 2" > nul 2>&1
-echo Please do consider to donate and share this tool
-PowerShell -Command "Start-Sleep -s 1" > nul 2>&1
-echo Donate Link: fawazahmed0.github.io/donate.html
-echo.
+
 :: Launching folder in current directory
 start .
 
