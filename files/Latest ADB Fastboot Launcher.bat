f06feca74dadc45d9efae3d89790db1362f39a7d
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-08-14 16:46:15 +0530

    Update Latest ADB Fastboot Launcher.bat

--- files/Latest ADB Fastboot Launcher.bat
+++ files/Latest ADB Fastboot Launcher.bat
@@ -5,7 +5,7 @@ cd "%HOMEPATH%\Latest ADB Fastboot Tool"  > nul 2>&1
 
 echo.
 echo Paste your files(twrp,etc) in Latest ADB Fastboot Tool Folder
-echo And enter ADB and Fastboot commands in here
+echo and enter ADB and Fastboot commands in here
 echo.
 
 PowerShell -Command "Start-Sleep -s 2" > nul 2>&1
@@ -15,8 +15,3 @@ start .
 :: https://stackoverflow.com/a/60907/2437224
 :: Stops the command prompt from getting closed
 cmd /k
-
-
-
-
-
