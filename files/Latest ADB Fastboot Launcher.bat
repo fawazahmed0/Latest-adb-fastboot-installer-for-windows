cc358d54dfa80fb862641744e408b143f79965d6
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-08-14 17:10:17 +0530

    Add donate msg in launcher

--- files/Latest ADB Fastboot Launcher.bat
+++ files/Latest ADB Fastboot Launcher.bat
@@ -9,6 +9,10 @@ echo and enter ADB and Fastboot commands in here
 echo.
 
 PowerShell -Command "Start-Sleep -s 2" > nul 2>&1
+echo Please do consider to donate and share this tool
+PowerShell -Command "Start-Sleep -s 1" > nul 2>&1
+echo Donate Link: fawazahmed0.github.io/donate.html
+echo.
 :: Launching folder in current directory
 start .
 
